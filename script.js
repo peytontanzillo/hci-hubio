438d79f18354447fc2c35d04bc7453670c4f230c
Samir Kashyap <samir.kashyap17@gmail.com>
2018-10-15 17:54:47 -0500

    Added minimal functionality to search function

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
+console.log("This is working");
+
 function openVideo(Title, season, episode){
   // do some stuff where it opens a new page and displays the corresponding video from the database
 }
 
-function sortbyGenre(genre){
+function sortbyGenre(genre){ // returns results that are the selected genre
   var results = [];
   if (show.genre === genre){
     results.add(show);
   }
   return results;
-}
-
-function search(){
-  var searchResult = document.getElementById('search-box').value;
-  console.log(searchResult);
-
 }
