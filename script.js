04b495d1fa2829ccc3dff421b5679b9c4b4517ff
Samir Kashyap <samir.kashyap17@gmail.com>
2018-10-16 16:02:48 -0500

    Added method to try to get content from logged in services

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-console.log("This is working");
+console.log("js file starts");
+
+var loginservices = [];
+// get the logged in services
+//
+// show content on homescreen from the logged in services
+// var content = [];
+// for (service in loginservices){
+//  if (tvshow.service === service){
+//    content.add;
+//  }
+//}
+
 
 function openVideo(Title, season, episode){
   // do some stuff where it opens a new page and displays the corresponding video from the database
@@ -11,3 +23,4 @@ function sortbyGenre(genre){ // returns results that are the selected genre
   }
   return results;
 }
+console.log(results);
