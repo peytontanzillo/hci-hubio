c30d07c9aa23839b9bdaf8284c5071f6727c8fd2
Joey Ballentine <joeyjballentine@gmail.com>
2018-10-17 22:02:26 -0500

    Added ability to add TV Shows and Movies

--- script.js
+++ script.js
@@ -23,4 +23,28 @@ function sortbyGenre(genre){ // returns results that are the selected genre
   }
   return results;
 }
-console.log(results);
+//console.log(results);
+
+
+function TvShow(title, rating, description, seasons) {
+  this.title = title;
+  this.rating = rating;
+  this.description = description;
+  this.seasons = seasons;
+}
+
+function Season(NumberOfEpisodes) {
+  this.NumberOfEpisodes = NumberOfEpisodes;
+}
+
+function Movie(title, rating, description) {
+  this.title = title;
+  this.rating = rating;
+  this.description = description;
+}
+
+let OfficeSeasons = [new Season(25), new Season(20), new Season(26), new Season(25)];
+let TheOffice = new TvShow("The Office", 10, "The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company. To simulate the look of an actual documentary, it was filmed in a single-camera setup, without a studio audience or a laugh track.", OfficeSeasons)
+
+console.log(TheOffice.title);
+console.log(TheOffice.seasons);
