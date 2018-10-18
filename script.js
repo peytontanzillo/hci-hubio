9b5cad559977b68405b2d6f2b833a55a4b04b866
Joey Ballentine <joeyjballentine@gmail.com>
2018-10-18 15:41:04 -0500

    Added scroll buttons and some other changes
    Also added more tv shows

--- script.js
+++ script.js
@@ -24,13 +24,20 @@ function sortbyGenre(genre){ // returns results that are the selected genre
 //console.log(results);
 
 $(document).ready(function() {
-    function TvShow(title, rating, description, seasons, services, imgPath) {
+    function TvShow(title, rating, description, genre, year, seasons, services, imgPath) {
+      // Metadata
       this.title = title;
       this.rating = rating;
       this.description = description;
+      this.genre = genre;
+      this.year = year;
+
+      // Arrays
       this.seasons = seasons;
-      this.imgPath = imgPath;
       this.services = services;
+
+      //  Image path
+      this.imgPath = imgPath;
     }
 
     function Season(NumberOfEpisodes) {
@@ -44,27 +51,57 @@ $(document).ready(function() {
       this.services = services;
     }
 
-    let TheOffice = new TvShow("The Office", 10,
-      "The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company. To simulate the look of an actual documentary, it was filmed in a single-camera setup, without a studio audience or a laugh track."
-      , [new Season(25), new Season(20), new Season(26), new Season(25)],
-      ["Netflix"], "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2015/03/the_office_season_1_cast.jpg");
+    let TheOffice = new TvShow(
+      "The Office",
+      10,
+      "comedy",
+      "2005",
+      "The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company. To simulate the look of an actual documentary, it was filmed in a single-camera setup, without a studio audience or a laugh track.",
+      [new Season(25), new Season(20), new Season(26), new Season(25)],
+      ["Netflix"],
+      "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2015/03/the_office_season_1_cast.jpg");
 
-    let SouthPark = new TvShow("South Park", 8,
-      "The animated series is not for children. In fact, its goal seems to be to offend as many as possible as it presents the adventures of Stan, Kyle, Kenny and Cartman."
-      , [new Season(12), new Season(12), new Season(12), new Season(12)],
-      ["Hulu"], 'Content/forest.jpg');
+    let SouthPark = new TvShow(
+      "South Park",
+      8,
+      "comedy",
+      "1997",
+      "The animated series is not for children. In fact, its goal seems to be to offend as many as possible as it presents the adventures of Stan, Kyle, Kenny and Cartman.",
+      [new Season(12), new Season(12), new Season(12), new Season(12)],
+      ["Hulu"],
+      "http://3.images.southparkstudios.com/default/image.jpg");
 
-    let DragonBallZ = new TvShow("Dragon Ball Z", 7,
-      "Dragon Ball Z follows the adventures of Goku who, along with the Z Warriors, defends the Earth against evil. The action adventures are entertaining and reinforce the concept of good versus evil. Dragon Ball Z teaches valuable character virtues such as teamwork, loyalty, and trustworthiness."
-      , [new Season(12), new Season(8), new Season(24), new Season(25)],
-      ["Hulu", "Crunchyroll"], 'Content/forest.jpg');
+    let DragonBallZ = new TvShow(
+      "Dragon Ball Z",
+      7,
+      "action",
+      "1989",
+      "Dragon Ball Z follows the adventures of Goku who, along with the Z Warriors, defends the Earth against evil. The action adventures are entertaining and reinforce the concept of good versus evil. Dragon Ball Z teaches valuable character virtues such as teamwork, loyalty, and trustworthiness.",
+      [new Season(12), new Season(8), new Season(24), new Season(25)],
+      ["Hulu", "Crunchyroll"],
+      'Content/forest.jpg');
 
-    let Dark = new TvShow("Dark", 10,
-      "When two children go missing in a small German town, its sinful past is exposed along with the double lives and fractured relationships that exist among four families as they search for the kids."
-      , [new Season(12)],
-      ["Netflix"], 'Content/forest.jpg');
+    let Dark = new TvShow(
+      "Dark",
+      10,
+      "sci-fi",
+      "2018",
+      "When two children go missing in a small German town, its sinful past is exposed along with the double lives and fractured relationships that exist among four families as they search for the kids.",
+      [new Season(12)],
+      ["Netflix"],
+      'Content/forest.jpg');
 
-    let TvShows = [TheOffice, SouthPark, DragonBallZ, Dark];
+    let WestWorld = new TvShow(
+      "West World",
+      5,
+      "sci-fi",
+      "2017",
+      "idk.",
+      [new Season(10)],
+      ["Prime Video"],
+      'Content/forest.jpg');
+
+    let TvShows = [TheOffice, SouthPark, DragonBallZ, Dark, WestWorld];
 
     for (var i = 0; i < TvShows.length; i++) {
       AddElement(TvShows[i], "#recommended");
@@ -75,4 +112,12 @@ $(document).ready(function() {
         let html = "<div class=\"content\" style=\"background: url(" + show.imgPath + "); background-position: center; background-size: cover;\"><h5 class=\"content-title\">" + show.title + "</h5><h4 class=\"content-service " + show.services[0] + "\">" + show.services[0] + "</h4></div>";
         $(location).append(html);
     };
+
+    $('#recommended-scroll-right').click(function() {
+        document.getElementById('recommended').scrollLeft += 100;
+      });
+    $('#recommended-scroll-left').click(function() {
+        document.getElementById('recommended').scrollLeft -= 100;
+      });
+
 });
