ccae06b7a5cabdf02a787690441ad8a28c4e85b2
Joey Ballentine <joeyjballentine@gmail.com>
2018-10-23 23:05:34 -0500

    Added peyton's content listener thing back in since i think i removed it

--- script.js
+++ script.js
@@ -272,36 +272,31 @@ $(document).ready(function() {
     let cameFromSearch = false;
 
     function AddElement(show, location) {
-        let html = "<a href=\"#\" id=\"" + show.id + "\" class=\"content\" style=\"background: url(" + show.imgPath + "); background-position: center; background-size: cover;\"><h5 class=\"content-title\">" + show.title + "</h5><h4 class=\"content-service " + show.services[0] + "\">" + show.services[0] + "</h4></div>";
-        $(location).append(html);
+      let html = "<a href=\"#\" id=\"" + show.id + "\" class=\"content\" style=\"background: url(" + show.imgPath + "); background-position: center; background-size: cover;\"><h5 class=\"content-title\">" + show.title + "</h5><h4 class=\"content-service " + show.services[0] + "\">" + show.services[0] + "</h4></div>";
+      $(location).append(html);
+      addContentListener(show);
+    }
 
-        $('#' + show.id).click(function() {
-            //var id = $(this).attr('id');
-            //let show = null;
+  function addContentListener(show) {
+      $(document).on('click', '#' + show.id, function() {
+          console.log('yite');
+          let html = "<div id=\"show-display\"><h5>" + show.title + "</h5></div>"
+          $("#show-title").empty().append(show.title);
+          $("#show-description").empty().append(show.description);
+          $("#show-description").empty().append(show.description);
+          $(".show-img-container").empty().append("<img src=\"" + show.imgPath + "\">");
+          $("#show-rating-year").empty().append("X Stars - 20XX");
+          if ($('.page-content').is(':visible')) {
+              $('.page-content').slideUp(300);
+              cameFromSearch = false;
+          } else {
+              $('.search-content').hide()
+              cameFromSearch = true;
+          }
+          $('.tv-show-page').show();
+      });
+    }
 
-            //    for (let i = 0; i < Medias.length; i++) {
-            //      if (Medias[i].id === id) {
-            //        show = Medias[i];
-            //      }
-            //    }
-
-            let html = "<div id=\"show-display\"><h5>" + show.title + "</h5></div>"
-            $("#show-title").empty().append(show.title);
-            $("#show-description").empty().append(show.description);
-            $("#show-description").empty().append(show.description);
-            $(".show-img-container").empty().append("<img src=\"" + show.imgPath + "\">");
-            $("#show-rating-year").empty().append("X Stars - 20XX");
-            if ($('.page-content').is(':visible')) {
-                $('.page-content').slideUp(300);
-                cameFromSearch = false;
-            } else {
-                $('.search-content').hide()
-                cameFromSearch = true;
-            }
-            $('.tv-show-page').show();
-        });
-
-    };
 
     let ScrollAmount = 500;
 
